702a419d7be69630ac75fad2b32e1041d89e00e6
100522207 <100522207@alumnos.uc3m.es>
2025-07-18 17:58:02 +0200

    Update arch-hyprland.sh

--- arch-hyprland.sh
+++ arch-hyprland.sh
@@ -10,77 +10,101 @@ REGION="Europe/Madrid"
 EDITOR="vim"
 #------------------------------------------#
 
-echo "[+] Formateando disco..."
-wipefs -af $DISCO
-sgdisk -Zo $DISCO
+loadkeys es
+reflector --country Spain --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-echo "[+] Creando particiones..."
-parted $DISCO --script mklabel gpt \
- mkpart ESP fat32 1MiB 513MiB \
- set 1 esp on \
- mkpart primary ext4 513MiB 100%
+# Particionado automático
+echo "[+] Borrando y creando particiones en $DISCO..."
+sgdisk -Z "$DISCO"
+sgdisk -n 1:0:+512M -t 1:ef00 -c 1:"EFI" "$DISCO"
+sgdisk -n 2:0:0 -t 2:8300 -c 2:"Linux" "$DISCO"
 
-EFI="${DISCO}1"
-ROOT="${DISCO}2"
+# Formateo
+mkfs.fat -F32 "${DISCO}1"
+mkfs.ext4 "${DISCO}2"
 
-echo "[+] Formateando particiones..."
-mkfs.fat -F32 $EFI
-mkfs.ext4 -F $ROOT
+# Montaje
+mount "${DISCO}2" /mnt
+mkdir -p /mnt/boot/efi
+mount "${DISCO}1" /mnt/boot/efi
 
-echo "[+] Montando..."
-mount $ROOT /mnt
-mkdir /mnt/boot
-mount $EFI /mnt/boot
-
+# Instalación base
 echo "[+] Instalando sistema base..."
-pacstrap -K /mnt base linux linux-firmware vim networkmanager sudo git base-devel bash-completion man-db man-pages less
+pacstrap /mnt base base-devel linux linux-firmware networkmanager sudo git grub efibootmgr $EDITOR
 
-echo "[+] Generando fstab..."
+# fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt /bin/bash <<EOF
+# Contraseñas
+read -s -p "Contraseña para root: " rootpass; echo
+read -s -p "Confirma contraseña para root: " rootpass2; echo
+[[ "$rootpass" != "$rootpass2" ]] && echo "No coinciden." && exit 1
 
-echo "[+] Configurando zona horaria y locale..."
+read -s -p "Contraseña para $USUARIO: " userpass; echo
+read -s -p "Confirma contraseña para $USUARIO: " userpass2; echo
+[[ "$userpass" != "$userpass2" ]] && echo "No coinciden." && exit 1
+
+echo "root:$rootpass" > /mnt/rootpass
+echo "$USUARIO:$userpass" > /mnt/userpass
+unset rootpass rootpass2 userpass userpass2
+
+# Script en chroot
+arch-chroot /mnt /bin/bash <<EOF
+set -e
+echo "[+] Configurando zona horaria, idioma y red..."
 ln -sf /usr/share/zoneinfo/$REGION /etc/localtime
 hwclock --systohc
-
 echo "$LOCALE UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
-
-echo "[+] Configurando red..."
 echo "$HOSTNAME" > /etc/hostname
-cat >> /etc/hosts <<END
-127.0.0.1       localhost
-::1             localhost
-127.0.1.1       $HOSTNAME.localdomain $HOSTNAME
-END
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
-echo "[+] Instalando GRUB y configurando red..."
-pacman -S --noconfirm grub efibootmgr networkmanager $EDITOR
+# Contraseñas y usuario
+echo "[+] Aplicando contraseñas..."
+chpasswd < /rootpass
+useradd -m -G wheel -s /bin/bash "$USUARIO"
+chpasswd < /userpass
+rm /rootpass /userpass
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-systemctl enable NetworkManager
-
-echo "[+] Creando usuario..."
-useradd -mG wheel $USUARIO
-
-echo "[!] Establece la contraseña para root:"
-passwd
-
-echo "[!] Establece la contraseña para $USUARIO:"
-passwd $USUARIO
-
+# Sudo
 echo "[+] Habilitando sudo para grupo wheel..."
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-echo "[+] Instalando Hyprland y herramientas..."
-pacman -S --noconfirm hyprland kitty waybar dunst rofi network-manager-applet \
-thunar tumbler thunar-archive-plugin thunar-volman gvfs \
-xdg-user-dirs xdg-desktop-portal-hyprland \
-pipewire wireplumber pipewire-audio pipewire-pulse \
-pamixer brightnessctl wl-clipboard polkit-gnome ttf-jetbrains-mono neofetch unzip
+# GRUB
+echo "[+] Instalando GRUB..."
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# NetworkManager
+systemctl enable NetworkManager
+
+# Chaotic-AUR
+echo "[+] Añadiendo repositorio Chaotic-AUR..."
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U --noconfirm 'https://cdn.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo '[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf
+pacman -Sy
+
+# Hyprland y apps
+echo "[+] Instalando entorno Hyprland..."
+pacman -S --noconfirm hyprland-bin kitty waybar thunar network-manager-applet firefox ttf-font-awesome noto-fonts
+
+# Instalar neofetch desde AUR
+echo "[+] Instalando neofetch desde AUR (puede tardar)..."
+sudo -u "$USUARIO" bash <<INNER
+cd ~
+git clone https://aur.archlinux.org/neofetch.git
+cd neofetch
+makepkg -si --noconfirm
+cd ..
+rm -rf neofetch
+INNER
 
 EOF
 
