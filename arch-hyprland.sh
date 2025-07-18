5050fb575c7545a20f4b22eba7dc438f806f1a61
100522207 <100522207@alumnos.uc3m.es>
2025-07-18 17:24:06 +0200

    Repos oficiales, y se han añadido nuevos, como kitty y pipewire

--- arch-hyprland.sh
+++ arch-hyprland.sh
@@ -16,9 +16,9 @@ sgdisk -Zo $DISCO
 
 echo "[+] Creando particiones..."
 parted $DISCO --script mklabel gpt \
- mkpart ESP fat32 1MiB 512MiB \
+ mkpart ESP fat32 1MiB 513MiB \
  set 1 esp on \
- mkpart primary ext4 512MiB 100%
+ mkpart primary ext4 513MiB 100%
 
 EFI="${DISCO}1"
 ROOT="${DISCO}2"
@@ -33,13 +33,14 @@ mkdir /mnt/boot
 mount $EFI /mnt/boot
 
 echo "[+] Instalando sistema base..."
-pacstrap -K /mnt base linux linux-firmware vim networkmanager sudo git base-devel
+pacstrap -K /mnt base linux linux-firmware vim networkmanager sudo git base-devel bash-completion man-db man-pages less
 
 echo "[+] Generando fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt /bin/bash <<EOF
 
+echo "[+] Configurando zona horaria y locale..."
 ln -sf /usr/share/zoneinfo/$REGION /etc/localtime
 hwclock --systohc
 
@@ -47,6 +48,7 @@ echo "$LOCALE UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
 
+echo "[+] Configurando red..."
 echo "$HOSTNAME" > /etc/hostname
 cat >> /etc/hosts <<END
 127.0.0.1       localhost
@@ -54,37 +56,34 @@ cat >> /etc/hosts <<END
 127.0.1.1       $HOSTNAME.localdomain $HOSTNAME
 END
 
-echo "[+] Configurando usuario y sudo..."
-useradd -mG wheel $USUARIO
-echo "Establece contraseña para root:"
-passwd
-echo "Establece contraseña para $USUARIO:"
-passwd $USUARIO
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-echo "[+] Instalando grub y red..."
+echo "[+] Instalando GRUB y configurando red..."
 pacman -S --noconfirm grub efibootmgr networkmanager $EDITOR
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 
-echo "[+] Instalando Chaotic-AUR..."
-pacman -S --noconfirm curl
-curl -O https://aur.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst
-curl -O https://aur.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst
-pacman -U --noconfirm chaotic-keyring.pkg.tar.zst chaotic-mirrorlist.pkg.tar.zst
-echo -e "[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-pacman -Syu --noconfirm
+echo "[+] Creando usuario..."
+useradd -mG wheel $USUARIO
+
+echo "[!] Establece la contraseña para root:"
+passwd
+
+echo "[!] Establece la contraseña para $USUARIO:"
+passwd $USUARIO
+
+echo "[+] Habilitando sudo para grupo wheel..."
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 echo "[+] Instalando Hyprland y herramientas..."
 pacman -S --noconfirm hyprland kitty waybar dunst rofi network-manager-applet \
-thunar tumbler thunar-archive-plugin thunar-volman gvfs xdg-desktop-portal-hyprland \
-xdg-user-dirs pipewire wireplumber pipewire-audio pipewire-pulse \
+thunar tumbler thunar-archive-plugin thunar-volman gvfs \
+xdg-user-dirs xdg-desktop-portal-hyprland \
+pipewire wireplumber pipewire-audio pipewire-pulse \
 pamixer brightnessctl wl-clipboard polkit-gnome ttf-jetbrains-mono neofetch unzip
 
 EOF
 
-echo "[+] Listo!"
+echo "[+] Instalación finalizada. \nReiniciando..."
 umount -R /mnt
 reboot
